db92ca5b0157610b2a40659eecd2c0044f9e6021
Steven Adetomiwa <s.adetomiwa@icloud.com>
2023-04-09 21:31:20 -0500

    edit

--- app/blueprints/api/__init__.py
+++ app/blueprints/api/__init__.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
 
 
+
 api = Blueprint('api',__name__, url_prefix='/api')
 
-from . import routes
+from api import routes
+
+
+
--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,11 +1,16 @@
 from flask import request
+from app import db
 from . import api
 from app.models import Post, User
+from .auth import basic_auth, token_auth
 
 
-@api.route('/')
+@api.route('/token')
+@basic_auth.login_required
 def index():
-    return 'Hello this is the API'
+    user = basic_auth.current_user()
+    token = user.get_token()
+    return {'token': token, 'token_exp': user.token_expiration}
 
 
 # END TO TO GET ALL THE POST 
@@ -22,15 +27,16 @@ def get_post(post_id):
         return {'error': 'Post not found'}, 404
     return post.to_dict()
 
-
+#endpoint to create a new post
 @api.route('/posts', methods=['POST'])
+@token_auth.login_required
 def create_post():
     #check to see that the request is a json request
     if not request.is_json:
         return {'error': 'Request must be JSON'}, 400
     # get the data from the request
     data = request.json
-    required_fields = ['title', 'body', 'user_id']
+    required_fields = ['title', 'body']
     missing_fields = []
     for field in required_fields:
         if field not in data:
@@ -43,10 +49,10 @@ def create_post():
     title=data.get('title')
     body=data.get('body')
     image_url=data.get('image_url')
-    user_id=data.get('user_id')
+    user = token_auth.current_user()
 
     #Create a new Post instance with the data from the request
-    new_post = Post(title=title, body=body, image_url=image_url, user_id=user_id)
+    new_post = Post(title=title, body=body, image_url=image_url, user_id=user.id)
 
     #Return the new post as JSON response
     return new_post.to_dict(), 201
@@ -54,13 +60,13 @@ def create_post():
     return 'This is the create post route'
 
 
-@api.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return [user.to_dict() for user in users]
+# @api.route('/users', methods=['GET'])
+# def get_users():
+#     users = User.query.all()
+#     return [user.to_dict() for user in users]
 
 
-@api.route('/users/<int:user_id>', methods=['GET'])
+@api.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
     user = User.query.get(user_id)
     if user is None:
@@ -75,7 +81,7 @@ def create_user():
         return {'error': 'Request must be JSON'}, 400
     # get the data from the request
     data = request.json
-    required_fields = ['username', 'email', 'password', 'first_name', 'last_name']
+    required_fields = ['first_name', 'last_name','email', 'username',  'password']
     missing_fields = []
     for field in required_fields:
         if field not in data:
@@ -85,16 +91,21 @@ def create_user():
     
 
     #Get the data from request body
-    username=data.get('username')
-    email=data.get('email')
-    password=data.get('password')
-    first_name=data.get('first_name')
-    last_name=data.get('last_name')
+    first = data.get('first_name')
+    last = data.get('last_name')
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+
+
+    check_user = db.session.execute(db.select(User).filter((User.username == username)| (User.email== email))).scalars().all()
+    if check_user:
+        return {'error': 'User already exists'}, 400
+    
 
     #Create a new User instance with the data from the request
-    new_user = User(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+    new_user = User(first_name=first , last_name=last, email=email, username=username, password=password, )
 
     #Return the new user as JSON response
     return new_user.to_dict(), 201
-        
-    return 'This is the create user route'
+
